4ec687e56ec9ebb23b2ce093dac5f2da9b0c76d8
jsonsnow <1183010734@qq.com>
2020-12-29 11:32:24 +0800

    feat: image edit service

--- Mediator/Classes/ModuleService/WGImageEditModuleService.h
+++ Mediator/Classes/ModuleService/WGImageEditModuleService.h
@@ -15,6 +15,8 @@ static NSString *const kRouteImageEdit = @"//edit/imageEdit";
 static NSString *const kRouteImageEditParamImage = @"image";
 static NSString *const kRouteImageEditParamAutoDismiss = @"autoDismiss";
 static NSString *const kRouteImageEditParamDefaultColor = @"defaultColor";
+static NSString *const kRouteImageEditParamEditorCtr = @"editorCtr";
+static NSString *const kRouteImageEditParamNeedChangeOriention = @"needChangeOriention";
 
 @protocol WGImageEditModuleService <NSObject>
 
