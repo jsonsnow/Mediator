6e64ab5cffef1b020e3c27747b7e3f5d71c5f952
jsonsnow <1183010734@qq.com>
2020-12-29 16:57:49 +0800

    test

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
 read -p "输入提交信息：" commit
+sed -n "/s.version          = '[0-9].[0-9].[0-9]'/p" $1
 read -p "输入tag号：" tag
 
 echo "修改podspces的tag:  $tag"
-sed -i "s/s.version          = '[0-9].[0-9].[0-9]'/s.version          = '$tag'/" ./Mediator.podspec
+sed -i '' -e "s/s.version          = '[0-9].[0-9].[0-9]'/s.version          = '$tag'/" $1
 git add .
 git commit -m $commit
 git tag $tag
@@ -10,4 +11,4 @@ git push origin master
 git push --tags
 
 echo "正在推送到仓库"
-pod repo push CLSpces $podspcesPah --allow-warnings --sources='https://github.com/jsonsnow/CLSpces.git,https://github.com/CocoaPods/Specs.git'
+pod repo push CLSpces $1 --allow-warnings --sources='https://github.com/jsonsnow/CLSpces.git,https://github.com/CocoaPods/Specs.git'
