a7c2e12fc4d26e9bc0be1279d609ee01ef99615c
jsonsnow <1183010734@qq.com>
2021-01-16 20:56:05 +0800

    feat: update

---
+++ Mediator/Classes/ModuleService/WGWebModuleService.h
@@ -0,0 +1,28 @@
+//
+//  WGWebModuleService.h
+//  Mediator
+//
+//  Created by chen liang on 2021/1/7.
+//
+
+#ifndef WGWebModuleService_h
+#define WGWebModuleService_h
+
+#import <WGRouter/BifrostHeader.h>
+#import <WebKit/Webkit.h>
+//webview
+static NSString *const kRouteWebPath = @"//web/open";
+static NSString *const kRouteMQWebPath = @"//web/openMQ";
+static NSString *const kRouteWebUrlParams = @"url";
+static NSString *const kRouteWebScriptParams = @"script";
+
+
+@protocol WebModuleService <NSObject>
+@property (nonatomic, weak, readonly) WKWebView *webview;
+
+
+- (WKWebView *)webView;
+
+@end
+
+#endif /* WGWebModuleService_h */
